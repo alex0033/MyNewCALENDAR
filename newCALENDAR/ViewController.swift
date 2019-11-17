40596b1844c5b3a8a73775072712c048abab1682
神田有矢 <kandayuuya@kandayuuyanomac-mini.local>
2019-11-17 16:46:33 +0900

    Initial Commit

---
+++ newCALENDAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newCALENDAR
+//
+//  Created by 神田有矢 on 2019/11/17.
+//  Copyright © 2019年 神田有矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ newCALENDARTests/newCALENDARTests.swift
@@ -0,0 +1,34 @@
+//
+//  newCALENDARTests.swift
+//  newCALENDARTests
+//
+//  Created by 神田有矢 on 2019/11/17.
+//  Copyright © 2019年 神田有矢. All rights reserved.
+//
+
+import XCTest
+@testable import newCALENDAR
+
+class newCALENDARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
